a4ad88d5ce25e95d1a21a8c6a59e113c382d5c18
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-03 21:59:11 -0300

    Add createPost function

--- app/post.ts
+++ app/post.ts
@@ -9,6 +9,12 @@ export type Post = {
   title: string;
 };
 
+type NewPost = {
+  title: string;
+  slug: string;
+  markdown: string;
+};
+
 export type PostMarkdownAttributes = {
   title: string;
 };
@@ -63,4 +69,15 @@ export async function getPost(slug: string) {
   const html = marked(body);
 
   return { slug, html, title: attributes.title };
+}
+
+export async function createPost(post: NewPost) {
+  const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+
+  await fs.writeFile(
+    path.join(postsPath, post.slug + ".md"),
+    md
+  );
+
+  return getPost(post.slug);
 }
