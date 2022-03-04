33c946f8a4c21aeb8b9f7bc7e6358e28b270ad1d
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-03 21:30:25 -0300

    Create posts module

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,24 +1,9 @@
 import { Link, useLoaderData } from "remix";
 
-export type Post = {
-  slug: string;
-  title: string;
-};
+import { getPosts } from "~/post";
+import type { Post } from "~/post";
 
-export const loader = async () => {
-  const posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-  
-  return posts;
-};
+export const loader = async () => getPosts();
 
 export default function Posts() {
   const posts = useLoaderData<Post[]>();
