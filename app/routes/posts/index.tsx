e0cddc2b0f9f40cf74d3de230163a0b649f63ef9
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-03 21:02:43 -0300

    Add posts route "loader"

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,4 +1,21 @@
+import { useLoaderData } from "remix";
+
+export const loader = async () => [
+  {
+    slug: "my-first-post",
+    title: "My First Post",
+  },
+  {
+    slug: "90s-mixtape",
+    title: "A Mixtape I Made Just For You",
+  },
+];
+
 export default function Posts() {
+  const posts = useLoaderData();
+
+  console.log(posts);
+
   return (
     <main>
       <h1>Posts</h1>
